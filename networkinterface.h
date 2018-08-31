92738bf69b00cab62147c1024820b596024aea53
Jean-Francois Duval <jfduval@dephy.com>
2018-08-31 15:29:27 -0400

    Simpler constants, no more warnings for out of range chars

--- networkinterface.h
+++ networkinterface.h
@@ -1,24 +1,23 @@
 #ifndef NETWORKINTERFACE_H
 #define NETWORKINTERFACE_H
 
-const char EXO_POWER_CMD  = 0x4C;
-const char EXO_STREAM_CMD = 0x93;
-const char EXO_TIMESTAMP_CMD = 0x5D;
-const char EXO_EVENT_CMD = 0x0A;
+//Commands:
+const char EXO_STREAM_CMD = 0x01;
+const char EXO_POWER_CMD  = 0x02;
+const char EXO_TIMESTAMP_CMD = 0x03;
+const char EXO_EVENT_CMD = 0x04;
 
-const char POWER_HIGH = 0x7E;
-const char POWER_LOW = 0x81;
+//Parameters:
+const char STREAM_START = 0x11;
+const char STREAM_STOP = 0x12;
+const char POWER_HIGH = 0x13;
+const char POWER_LOW = 0x14;
 
-const char STREAM_START = 0x3A;
-const char STREAM_STOP = 0x83;
-
+//Settings & internal:
 const char COMMAND_LENGTH = 3;
-
 const int SERVER_TCP_PORT = 63367;
-
-const char CONNECTION_ACK = 0xEE;
-
-const unsigned char OK_RESPONSE[4] = "AOK";
-const unsigned char ERR_RESPONSE[4] = "ERR";
+const char CONNECTION_ACK = 0x7F;
+const char OK_RESPONSE[4] = "AOK";
+const char ERR_RESPONSE[4] = "ERR";
 
 #endif // NETWORKINTERFACE_H
